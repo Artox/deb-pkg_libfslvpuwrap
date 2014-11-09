9ab82d23829cebc434340b9db2266506c631f6fb
Josua Mayer <privacy@not.given>
2014-11-09 15:33:51 +0100

    create packaging

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libfslvpuwrap):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="libfslvpuwrap" command="/usr/bin/libfslvpuwrap"
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibfslvpuwrap 1.0.46 libfslvpuwrap (>> 1.0.46-0), libfslvpuwrap (<< 1.0.46-99)
