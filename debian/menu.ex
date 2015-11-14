1f20b64d7b4144bccfa7bdfc1fbaec725581689c
Josua Mayer <privacy@not.given>
2015-11-14 15:28:07 +0000

    add soname to package name

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libfslvpuwrap):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libfslvpuwrap" command="/usr/bin/libfslvpuwrap"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liblibfslvpuwrap 1.0.46 libfslvpuwrap (>> 1.0.46-0), libfslvpuwrap (<< 1.0.46-99)
